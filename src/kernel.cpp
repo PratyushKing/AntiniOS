76432d3d4d4bb199c9f1eb9aa8f10044de0a4f61
MarkAR2012 <159020473+MarkAR2012@users.noreply.github.com>
2024-06-01 19:31:00 +0100

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -5,7 +5,6 @@
 extern "C" void kernel_main(void)
 {
     init_terminal();
-    terminal_setcolor(VGA_COLOR_GREEN);
-    clear_scr_with_color(VGA_COLOR_BLUE);
-    terminal_writestring("Hello, AntiniOS!\n");
+    clear_scr_with_color_and_fore(VGA_COLOR_BLUE, VGA_COLOR_YELLOW);
+    terminal_writestring("Welcome to AntiniOS\n");
 }
